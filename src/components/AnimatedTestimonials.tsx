057e57e0407c81d5ae93c700f117d97f443f67ed
Abubaker779 <abubakaransari7798@gmail.com>
2025-05-14 15:15:34 +0530

    photos added inspector

--- src/components/AnimatedTestimonials.tsx
+++ src/components/AnimatedTestimonials.tsx
@@ -5,19 +5,31 @@ export function AnimatedTestimonialsDemo() {
     {
       quote:
         "",
-      name: "Dr. SRIMATI MANJU SIWACH",
-      designation: "MLA Modinagar",
+      name: "Dr. SHRIMATI MANJU SIWACH",
+      designation: "MLA, Modinagar",
       src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQECBi76FxIAlB527rTdEqG4vVqMZA4ghOudw&s",
     },
     {
       quote:
         "",
       name: "Shri Vinod Vaishali",
-      designation: "Chairman Nagar Palika Parishad Modinagar",
+      designation: "Chairman, Nagar Palika Parishad Modinagar",
       src: "https://i.ibb.co/GQ08SxH3/Whats-App-Image-2025-05-14-at-2-49-13-PM.jpg",
     },
-    
-      
+    {
+      quote:
+        "",
+      name: "Shri Umesh Chandra Anand",
+      designation: "(T.S) Nagar Palika Parishad Modinagar",
+      src: "https://i.ibb.co/R4txczTF/Screenshot-2025-05-14-150925.png",
+    },
+    {
+      quote:
+        "",
+      name: "Shri Ankit Chaudhary",
+      designation: "Revenue Inspector, Nagar Palika Parishad Modinagar",
+      src: "https://i.ibb.co/bRrzgcj3/Screenshot-2025-05-14-150906.png",
+    },  
    
   ];
   return <AnimatedTestimonials testimonials={testimonials} />;
