36111d38965b5cad93adec5b955e7e65266bfc71
Abubaker779 <abubakaransari7798@gmail.com>
2025-05-14 15:00:31 +0530

    img added quote fix 1

--- src/components/AnimatedTestimonials.tsx
+++ src/components/AnimatedTestimonials.tsx
@@ -5,14 +5,14 @@ export function AnimatedTestimonialsDemo() {
     {
       quote:
         "",
-      name: "Dr. SHRIMATI MANJU SIWACH BJP MLA",
+      name: "Dr. SRIMATI MANJU SIWACH",
       designation: "MLA Modinagar",
       src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQECBi76FxIAlB527rTdEqG4vVqMZA4ghOudw&s",
     },
     {
       quote:
         "",
-      name: "Vinod Vaishali",
+      name: "Shri Vinod Vaishali",
       designation: "Chairman Nagar Palika Parishad Modinagar",
       src: "https://i.ibb.co/GQ08SxH3/Whats-App-Image-2025-05-14-at-2-49-13-PM.jpg",
     },
